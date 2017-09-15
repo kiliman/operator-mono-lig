99dcee23f606c5affee40e1bee37bcaefc2e9e00
Kiliman <kiliman@gmail.com>
2017-09-15 19:03:44 -0400

    Update extract and build process for new weights

--- build.bat
+++ build.bat
@@ -1,13 +1,22 @@
 @echo off
 
-del .\original\*.ttx
+del /q .\original\*.ttx
+
 ttx .\original\OperatorMono-Medium.otf
 ttx .\original\OperatorMono-MediumItalic.otf
+ttx .\original\OperatorMonoSSm-Book.otf
+ttx .\original\OperatorMonoSSm-BookItalic.otf
+ttx .\original\OperatorMonoSSm-Medium.otf
+#ttx .\original\OperatorMonoSSm-MediumItalic.otf
 
 if not exist build\* mkdir build
 
 node index.js OperatorMono-Medium
 node index.js OperatorMono-MediumItalic
+node index.js OperatorMonoSSm-Book
+node index.js OperatorMonoSSM-BookItalic
+node index.js OperatorMonoSSm-Medium
+#node index.js OperatorMonoSSm-MediumItalic
 
 del /q .\build\*.otf
 
--- extract.bat
+++ extract.bat
@@ -1,4 +1,17 @@
 @echo off
 
+del /q .\ligature_source\*.ttx
+
+ttx .\ligature_source\OperatorMonoLig-Medium.otf
+ttx .\ligature_source\OperatorMonoLig-MediumItalic.otf
+ttx .\ligature_source\OperatorMonoSSmLig-Book.otf
+ttx .\ligature_source\OperatorMonoSSmLig-BookItalic.otf
+ttx .\ligature_source\OperatorMonoSSmLig-Medium.otf
+#ttx .\ligature_source\OperatorMonoSSmLig-MediumItalic.otf
+
 node extract.js OperatorMonoLig-Medium
 node extract.js OperatorMonoLig-MediumItalic
+node extract.js OperatorMonoSSmLig-Book
+node extract.js OperatorMonoSSmLig-BookItalic
+node extract.js OperatorMonoSSmLig-Medium
+#node extract.js OperatorMonoSSmLig-MediumItalic
