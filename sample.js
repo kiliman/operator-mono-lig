74ac6d5c900d1479141cb43ff035e5b94509c072
Kiliman <kiliman@gmail.com>
2017-09-16 08:59:03 -0400

    Add sample file

---
+++ sample.js
@@ -0,0 +1,23 @@
+// this is a comment
+const func = (a, b, c, d, e, f, s, n) => {
+    if (a++ && b-- || c >= d || e <= f) {      
+    }
+    if (s === 'string' && n !== 999) {
+    }
+    if (a == b && c != d)
+    
+    a <- b <-- c <== d
+
+    a <| b |> c -> d ==> e 
+    
+  }
+  <!-- test -->
+
+  // italic ligatures
+  // != == >= <= -> => ==>
+  // === !== && || ++ --
+  
+  
+  
+  
+  
