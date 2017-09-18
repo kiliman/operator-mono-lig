1c2d8b4d8f4c7b565dd3ae484ee6b6b783fdbf58
kiliman <kiliman@gmail.com>
2017-09-18 12:48:39 -0400

    Make sure charstrings.xml exists for font before building

--- build.bat
+++ build.bat
@@ -19,6 +19,7 @@ set lig=%1
 set otf=%lig:Lig=%
 
 if not exist .\original\%otf%.otf exit /b
+if not exist .\ligature\%lig%\charstrings.xml exit /b
 
 @echo Building %lig%
 ttx -f .\original\%otf%.otf
