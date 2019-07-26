22ebd8aab711dcaca66289d9d75e7592b72a3b53
Kiliman <kiliman@gmail.com>
2019-07-26 16:33:47 -0400

    📝 Update README for v2.0

---
+++ newligatures.go
@@ -0,0 +1,21 @@
+// new ligatures
+less_slash.liga             </
+slash_greater.liga          />
+underscore_underscore.liga  __
+colon_equal.liga            :=
+
+// alternate ligature glyphs
+greater_equal.2.liga     >=
+less_equal.2.liga        <=
+equal_equal.2.liga       ==  // extended equal with gap
+equal_equal_equal.2.liga === // extended equal with gap
+
+// non-ligature glyphs (not by default, use profiles.ini)
+percent=percent.alt      % 	// open vs closed circles
+
+// power line glyphs
+uniE0A0 			// Powerline branch
+uniE0B0 	 	// Powerline right triangle
+uniE0B2 	 	// Powerline left triangle
+
+
