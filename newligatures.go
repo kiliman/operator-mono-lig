4cf344a4357702b8c0299fb56d508f0480dd7598
kiliman <kiliman@gmail.com>
2019-10-31 14:13:31 -0400

    🚀 Deploy v2.2.7 with less_hyphen_greater ligature

--- newligatures.go
+++ newligatures.go
@@ -4,6 +4,7 @@ slash_greater.liga          />
 underscore_underscore.liga  __
 colon_equal.liga            :=
 w_w_w.liga									www
+less_hyphen_greater.liga		<->
 
 // alternate ligature glyphs
 greater_equal.2.liga     >=
@@ -18,5 +19,3 @@ percent=percent.alt      % 	// open vs closed circles
 uniE0A0 			// Powerline branch
 uniE0B0 	 	// Powerline right triangle
 uniE0B2 	 	// Powerline left triangle
-
-
